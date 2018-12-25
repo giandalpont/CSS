72bdac153dd3bbba22b82a9512900625aa20a6f4
Gian Carlos Dal Pont <giandalpont@gmail.com>
2018-12-24 23:05:41 -0200

    variáveis e operadores

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,3 +1,36 @@
-console.log('Olá, sou um console!');
-//TODO: alert('Gian');
+// console.log('Olá, sou um console!');
+//TODO: alert('Sou um alert');
 //TODO: document.write('<h1>Olá meu povo</h1>');
+
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Cadastro Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 300) {
+  // console.log('peso inválido!');
+  pesoEhValido = false;
+  tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+  // console.log("Altura inválida!");
+  alturaEhValida = false;
+  tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
