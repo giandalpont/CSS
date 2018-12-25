2ed269410af10d7363f3030c16db989e1ca41f7a
Gian Carlos Dal Pont <giandalpont@gmail.com>
2018-12-24 23:49:12 -0200

    array, loop e estilo

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -5,32 +5,38 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Cadastro Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 300) {
-  // console.log('peso inválido!');
-  pesoEhValido = false;
-  tdImc.textContent = "Peso inválido!";
-}
+  var pesoEhValido = true;
+  var alturaEhValida = true;
 
-if(altura <= 0 || altura >= 3.00){
-  // console.log("Altura inválida!");
-  alturaEhValida = false;
-  tdImc.textContent = "Altura inválida!";
-}
+  if (peso <= 0 || peso >= 300) {
+    // console.log('peso inválido!');
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+    paciente.classList.add("estilo-invalido");  // paciente.style.backgroundColor = "lightcoral";
+  }
 
-if (alturaEhValida && pesoEhValido) {
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
+  if(altura <= 0 || altura >= 3.00){
+    // console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+    paciente.classList.add("estilo-invalido");  // paciente.style.backgroundColor = "lightcoral";
+  }
+
+  if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
